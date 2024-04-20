4d45b03e630f5fb1ddcc3db5cc131c63c1fc099e
artem.iushin <82830797+Br33zes@users.noreply.github.com>
2024-04-20 13:37:49 +0500

    Add Function/dbo.udf_getSQUPrice.sql

---
+++ Function/dbo.udf_GetSKUPrice.sql
@@ -0,0 +1,12 @@
+create or alter function dbo.udf_GetSKUPrice(
+	@ID_SKU int
+)
+returns numeric(18, 2)
+as
+begin
+	return (
+	select sum(b.[Value]) / sum(b.Quantity)
+	from dbo.Basket as b
+	where b.ID_SKU = @ID_SKU
+	)
+end
